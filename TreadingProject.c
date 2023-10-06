fe5fb4809b09692b34fea995c9a6f8d28938130c
saadrafi <saadrafi81@gmail.com>
2023-10-06 16:26:49 +0600

    initial look

---
+++ TreadingProject.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct stock_node
+{
+    char name[20];
+    float buy_price;
+    float sell_price;
+    int quantity;
+    struct stock_node *next;
+} Stock;
+
+
+
+void main()
+{
+    int admin_choice, user_choice, choice;
+    do
+    {
+        printf("***********Welcome to Stock Management System************\n");
+        printf("*\t\t1. Admin Side\t\t\t\t*\n");
+        printf("*\t\t2. User Side\t\t\t\t*\n");
+        printf("*\t\t0. Exit\t\t\t\t\t*\n");
+        printf("*********************************************************\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            do
+            {
+                system("cls");
+                printf("**************Welcome to Admin Side**************\n");
+                printf("*\t\t1. Add Stock\t\t\t*\n");
+                printf("*\t\t2. Update Stock\t\t\t*\n");
+                printf("*\t\t3. Delete Stock\t\t\t*\n");
+                printf("*\t\t4. View Stock\t\t\t*\n");
+                printf("*\t\t0. Exit\t\t\t\t*\n");
+                printf("*************************************************\n");
+                printf("Enter your choice: ");
+                scanf("%d", &admin_choice);
+                switch (admin_choice)
+                {
+                case 1:
+                    
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    
+                    break;
+                case 0:
+                    break;
+                default:
+                    printf("Invalid Choice\n");
+                    break;
+                }
+            } while (admin_choice != 0);
+            break;
+        case 2:
+            do
+            {
+                printf("***********Welcome to User Side***********\n");
+                printf("*\t\t1. Buy Stock\t\t\t*\n");
+                printf("*\t\t2. Sell Stock\t\t\t*\n");
+                printf("*\t\t3. View Stock\t\t\t*\n");
+                printf("*\t\t0. Exit\t\t\t\t*\n");
+                printf("*******************************************\n");
+                printf("Enter your choice: ");
+                scanf("%d", &user_choice);
+                switch (user_choice)
+                {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 0:
+                    break;
+                default:
+                    printf("Invalid Choice\n");
+                    break;
+                }
+            } while (user_choice != 0);
+            break;
+        case 0:
+            break;
+        default:
+            printf("Invalid Choice\n");
+            break;
+        }
+
+    } while (choice != 0);
+}
+
+
